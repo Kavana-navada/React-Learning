5b35e3d868ee8a1b1e4ae65ca67f9aefa06d5fb3
Kavana Navada <kavananavada03@gmail.com>
2024-09-29 19:46:13 +0530

    used handle event

--- src/Button/Button.jsx
+++ src/Button/Button.jsx
@@ -12,10 +12,24 @@ function Button(){
         cursor: "pointer",
         
     };
+    let count=0;
+    const handleClick=(e)=>e.target.textContent="Read";
+    const handleClick2=(name)=>
+        {
+            if(count<3)
+            {
+                count+=1;
+                console.log(`${name} ,u clicked ${count}times!!`);
+            }
+            else
+            {
+                console.log(`${name},stop u already clicked ${count}times`);
+            }
+        };
     return(
         <>
-        <button className={mystyles.instructionButton}> Click here to read instructions</button>
-        <button style={gobtn}> Let's go</button>
+        <button onClick={(e)=>handleClick(e)} className={mystyles.instructionButton}> Click here to read instructions</button>
+        <button onClick={()=>handleClick2("hey")} style={gobtn}> Let's go</button>
         </>
     );
 }
