b90cac13f7de30e82d98914cf7af11041d8d552f
Kavana Navada <kavananavada03@gmail.com>
2024-10-11 22:34:36 +0530

    used useState

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,5 +1,10 @@
+import { useState } from "react";
 import travelPic from "./assets/travel.jpg"
 function Card(){
+    const [color,setColor]=useState("#ffffff");
+    function handleColor(event){
+        setColor(event.target.value);
+    }
     return(
         <>
         <div className="card">
@@ -8,7 +13,14 @@ function Card(){
             <p className="card-text">Before you run out of time</p>
 
         </div>
-        <div className="card">
+        <div className="card" style={{backgroundColor:color}}>
+
+            <h3>Pick the Color of your vehicle </h3>
+            <div className="color-display" >
+                <p>Selected color:{color}</p>
+                <input type="color" value={color} onChange={handleColor}/>
+
+            </div>
 
         </div>
         </>
--- src/PlayTime.jsx
+++ src/PlayTime.jsx
@@ -1,9 +1,26 @@
 import React,{useState} from "react";
 function PlayTime()
 {
+    const [details,setDetails]=useState({name:"Guest",phone:0,city:"NIL"})
+    function handleNameChange(event){
+        setDetails({...details,name:event.target.value});
+
+    }
+
+    function handlePhoneChange(event){
+        setDetails({...details,phone:event.target.value});
+    }
+
+    function handleCityChange(event){
+        setDetails({...details,city:event.target.value});
+    }
+    
+    
     const [time, setTime]=useState(0);
     const IncreaseTimes =()=>{
-        setTime(time+1);
+        setTime(ti=>ti+1);
+        setTime(ti=>ti+1);
+
     }
     const DecreaseTimes =()=>{
         setTime(time-1);
@@ -15,6 +32,12 @@ function PlayTime()
     }
     return(
         <div>
+            <p>Your details:{details.name} &nbsp; {details.phone} {details.city}</p>
+            <label>Enter Name:</label><input type="text" value={details.name} onChange={handleNameChange}/>
+            <label>Enter Phone Number:</label><input type="number" value={details.phone} onChange={handlePhoneChange}/>
+            <label>Enter City:</label><input type="text" value={details.city} onChange={handleCityChange}/>
+
+
             <p>Have u went to trip along with your family? {went?"Yes":"No"}</p>
             <button onClick={yesNo}> Yes / No</button>
             <p>How many times:{time}</p>
